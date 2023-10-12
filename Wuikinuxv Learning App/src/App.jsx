3c6843b6718b516064896b9b36550761e1a86cd8
Trev004 <71028998+Trev004@users.noreply.github.com>
2023-10-11 18:33:07 -0700

    Add logo image to navbar
    includes proper CSS styling

--- Wuikinuxv Learning App/src/App.jsx
+++ Wuikinuxv Learning App/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <>
       <header>
         <nav>
+          <img src="Thumbnail_Uikala_LOGO.png" alt="Wuikinuxv nation logo" />
           <h1>Wuikinuxv Language App</h1>
         </nav>
       </header>
