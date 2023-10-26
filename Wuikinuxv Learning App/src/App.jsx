db43e1faa5d5a3fffd5cf6e78c620fa710c5daff
Trev004 <71028998+Trev004@users.noreply.github.com>
2023-10-25 18:00:17 -0700

    Add a style toggle to word area
    Currently doesn't have the other style coded, but right now, the button will toggle classes that will then apply CSS based on the desired view of the user. This was created with the help of (but not solely by of course) ChatGPT

--- Wuikinuxv Learning App/src/App.jsx
+++ Wuikinuxv Learning App/src/App.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react'
 import wordlist from './assets/words.json'
 import './App.css'
 import Header from './assets/Components/Header.jsx'
 
 function App() {
   const words = wordlist.words
+
+  const [isListView, setIsListView] = useState(true)
+
+  const viewChanger = () => {
+    setIsListView(!isListView)
+  }
+
+  const currentContainerView =  `${isListView ? 'wordarea-list' : 'wordarea-cards'}`;
+  const currentWordView = `${isListView ? 'word-list' : 'word-card'}`;
+
+  const buttonHandler = () => {
+    viewChanger();
+  }
+  // Modified from code created with the help of ChatGPT
+  useEffect(() => {
+    const button = document.getElementById('viewToggler');
+    button.addEventListener('click', buttonHandler);
+    return () => {
+      button.removeEventListener('click', buttonHandler);
+    };
+  },);
 
   return (
     <>
@@ -18,9 +40,10 @@ function App() {
         <p>If the symbol &nbsp; ̌  appears on top of a letter it&apos;s called a back sign and indicates that the sound is pronounced further back in throat.</p>
         <p>If the letter v appears behind a letter, it indicates that you pronounce the previous symbol with a rounded mouth so for example, gv would sound like a g, but with a rounded mouth. <br/>If the stress sign appears over a vowel, as in &quot;à&quot; it means it&apos;s given more emphasis over other vowels, it&apos;s usually the first vowel of a word.</p>
       </div>
-      <div className="wordarea">
+      <button id='viewToggler'>{isListView ? 'Toggle to Card View' : 'Toggle to List View'}</button>
+      <div className={currentContainerView}>
         {words.map((word, index) => (
-          <div key={index} className='word'>
+          <div key={index} className={currentWordView}>
             <audio controls>
               <source src={word.file} type="audio/mpeg" />
             </audio>
