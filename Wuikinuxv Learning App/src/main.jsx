5e8771d5ee787d7fbf32107355074934312ce6fc
Trev004 <71028998+Trev004@users.noreply.github.com>
2023-10-25 21:50:37 -0700

    Setup beginnings of React Router
    Currently following https://youtu.be/Ul3y1LXxzdU?si=vjxNY3vSal1qKQQP

--- Wuikinuxv Learning App/src/main.jsx
+++ Wuikinuxv Learning App/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './assets/Pages/App.jsx'
+import Header from './assets/Components/Header.jsx'
+import { BrowserRouter } from "react-router-dom"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
   </React.StrictMode>,
 )
