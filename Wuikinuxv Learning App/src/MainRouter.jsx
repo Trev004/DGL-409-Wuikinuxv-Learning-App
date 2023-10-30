c1d9f28fc28ca2ba89196e5711d95372b1cc15c5
Trev004 <71028998+Trev004@users.noreply.github.com>
2023-10-30 13:54:32 -0700

    Update Header links
    Change styling so they actually look like buttons, with respective stylings for light/dark mode, hover states, and active. Additionally, this commit will change the `Link` tag in MainRouter.jsx to a `NavLink`, allowing the links to automatically apply the active state to themselves

--- Wuikinuxv Learning App/src/MainRouter.jsx
+++ Wuikinuxv Learning App/src/MainRouter.jsx
@@ -1,7 +1,7 @@
 import App from './assets/Pages/App'
 import Flashcard from './assets/Pages/Flashcard'
 import './MainRouter.css'
-import { Link, Outlet, Route, Routes } from 'react-router-dom'
+import { NavLink, Outlet, Route, Routes } from 'react-router-dom'
 function MainRouter() {
     return (
     <>
@@ -9,8 +9,8 @@ function MainRouter() {
           <nav>
             <h1>Wuikinuxv Language App</h1>
             <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to={"/flashcards"}>Flashcards</Link></li>
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to={"/flashcards"}>Flashcards</NavLink></li>
             </ul>
           </nav>
       </header>
