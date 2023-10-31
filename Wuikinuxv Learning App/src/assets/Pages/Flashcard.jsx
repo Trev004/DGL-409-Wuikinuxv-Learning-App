45bbbcfd627d6ac3b874952cd0ff998a824f220f
Trev004 <71028998+Trev004@users.noreply.github.com>
2023-10-31 10:57:49 -0700

    Import word JSON into flashcard
    Doesn't have any flashcard logic yet, but shows how I may select one (soon to be) random word *from* the JSON (as opposed to how it worked on the main page, where I dynamically wanted the ENTIRE wordlist rendered, not just one random one as I would want here)

--- Wuikinuxv Learning App/src/assets/Pages/Flashcard.jsx
+++ Wuikinuxv Learning App/src/assets/Pages/Flashcard.jsx
@@ -1,7 +1,11 @@
+import wordlist from '../words.json'
+
 function Flashcard () {
+    console.log(wordlist)
     return(
         <>
             <h1>Test Content for now!</h1>
+            <p>{wordlist.words[1].word}</p>
         </>
     );
 }
