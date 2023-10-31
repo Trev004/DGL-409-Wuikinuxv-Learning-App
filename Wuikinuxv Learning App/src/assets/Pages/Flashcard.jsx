4b9d156ecd1213a3ab4791331190152640cf9fe7
Trev004 <71028998+Trev004@users.noreply.github.com>
2023-10-31 15:58:54 -0700

    Improve random number generator
    Now will avoid picking the number it had previously so it won't generate two 1's in row for example. Also implements basic CSS and page text

--- Wuikinuxv Learning App/src/assets/Pages/Flashcard.jsx
+++ Wuikinuxv Learning App/src/assets/Pages/Flashcard.jsx
@@ -1,15 +1,24 @@
 import wordlist from '../words.json'
 import { useState } from 'react';
+import './Flashcard.css'
 function Flashcard () {
     const words = wordlist.words
     const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() * words.length));
+    const [oldRandom, setOldRandom] = useState();
+
+    // Generates new randoms, repeats the loop if previous number is generated
     const newRandomNumber = () =>{
-        setRandomNumber(Math.floor(Math.random() * words.length));
+        let newRandom;
+        do {
+            newRandom = Math.floor(Math.random() * words.length);
+        } while (newRandom == oldRandom);
+        setRandomNumber(newRandom);
+        setOldRandom(randomNumber);
     }
     return(
         <>
-            <h1>Test Content for now!</h1>
-            <p>n is {randomNumber}</p>
+            <h2>Flashcards</h2>
+            <p>Click on the correct meaning of the word displayed</p>
             <button onClick={newRandomNumber}>New Random</button>
             <p>Word: {words[randomNumber].word} Meaning: {words[randomNumber].meaning}</p>
         </>
