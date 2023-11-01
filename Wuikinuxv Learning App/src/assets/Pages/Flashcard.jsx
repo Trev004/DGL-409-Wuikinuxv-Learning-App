828b05e20484703ca7e65a5d31bb7f2e2704e285
Trev004 <71028998+Trev004@users.noreply.github.com>
2023-10-31 19:33:50 -0700

    Add basic functionality to flashcard logic
    Draws 3 additional words for the purpose of having some wrong options to choose from, currently has very basic response detection. Meaning it'll detect whether or not the user clicked the correct response, but currently only displays a <p> tag, doesn't remove it upon clicking a new random word, and doesn't auto-randomize to a new word upon success.

--- Wuikinuxv Learning App/src/assets/Pages/Flashcard.jsx
+++ Wuikinuxv Learning App/src/assets/Pages/Flashcard.jsx
@@ -5,6 +5,8 @@ function Flashcard () {
     const words = wordlist.words
     const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() * words.length));
     const [oldRandom, setOldRandom] = useState();
+    const [wrongWords, setWrongWords] = useState([]);
+    const [userAnswer, setUserAnswer] = useState(null)
 
     // Generates new randoms, repeats the loop if previous number is generated
     const newRandomNumber = () =>{
@@ -14,13 +16,50 @@ function Flashcard () {
         } while (newRandom === oldRandom);
         setRandomNumber(newRandom);
         setOldRandom(newRandom);
+
+        // Generate 3 random indices for incorrect meanings
+        // GPT-Assisted
+        const incorrectIndices = [];
+        while (incorrectIndices.length < 3) {
+            const index = Math.floor(Math.random() * words.length);
+            if (index !== newRandom && !incorrectIndices.includes(index)) {
+                incorrectIndices.push(index);
+            }
+        }
+
+        const correctWord = words[newRandom].meaning;
+        const incorrectWords = incorrectIndices.map(index => words[index].meaning);
+
+        const allWords = [correctWord, ...incorrectWords];
+        shuffleWords(allWords);
+        setWrongWords(allWords);
+    }
+    // Array "shuffle" function from:
+    // https://www.freecodecamp.org/news/how-to-shuffle-an-array-of-items-using-javascript-or-typescript/
+    const shuffleWords = (arr) =>{
+        return arr.sort(() => Math.random() - 0.5);
+    }
+
+    const answerHandler = (meaning) => {
+        if (meaning === words[randomNumber].meaning) {
+            setUserAnswer(true);
+        } else {
+            setUserAnswer(false);
+        }
     }
     return(
         <>
             <h2 className='flashcard-intro'>Flashcards</h2>
             <p>Click on the correct meaning of the word displayed</p>
             <button onClick={newRandomNumber}>New Random</button>
-            <p>Word: {words[randomNumber].word} Meaning: {words[randomNumber].meaning}</p>
+            <p>Word: {words[randomNumber].word}</p>
+            <div className="flashcard-meanings">
+                {wrongWords.map((meaning, index) => (
+                    <button key={index} onClick={() => answerHandler(meaning)} className='flashcard-response'>{meaning}</button>
+                ))}
+            </div>
+            {userAnswer === true && <p>Correct!</p>}
+            {userAnswer === false && <p>Incorrect. Try again.</p>}
         </>
     );
 }
