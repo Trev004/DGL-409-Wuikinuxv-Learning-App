f640f33a40b64eeae46c624c6f60c6dc9d37d6b5
Trev004 <71028998+Trev004@users.noreply.github.com>
2023-11-22 20:59:07 -0800

    Add tooltips of word parts based on JSON
    Currently adds a title containing the `tip` to each `span` tag based on JSON.
    Also renamed the `sounds` object to `baseSounds` to better distinguish it from the sounds array for each word.
    Currently doesn't seem to want to do certain special characters such as ə̀

--- Wuikinuxv Learning App/src/assets/Components/Word.jsx
+++ Wuikinuxv Learning App/src/assets/Components/Word.jsx
@@ -1,14 +1,21 @@
 import { PropTypes } from "prop-types";
+import wordsJSON from '../words.json'
 
 function Word ({ word, isListView }) {
     const { sounds, meaning, file } = word; 
-    
+    const baseSounds = wordsJSON.baseSounds;
+
+    const getSoundTip = (sound) => {
+        const soundData = baseSounds.find((item) => item.sound === sound);
+        return soundData ? soundData.tip : '';
+    };
+      
     return (
       <div className={isListView ? 'word-list' : 'word-card'}>
         <audio controls>
           <source src={file} type="audio/mpeg" />
         </audio>
-        <h3>Word: {sounds.map((sound, index) => <span key={index}>{sound}</span>)}</h3>
+        <h3>Word: {sounds.map((sound, index) => <span key={index} title={getSoundTip(sound)}>{sound}</span>)}</h3>
         <p>Meaning: {meaning}</p>
       </div>
     );
@@ -17,11 +24,16 @@ function Word ({ word, isListView }) {
 // propTypes based on eslint recommendation and GPT-Assisted code
 Word.propTypes = {
     word: PropTypes.shape({
-      sounds: PropTypes.arrayOf(PropTypes.string).isRequired,
+      sounds: PropTypes.arrayOf(
+        PropTypes.shape({
+          sound: PropTypes.string.isRequired,
+          file: PropTypes.string.isRequired,
+        })
+      ).isRequired,
       meaning: PropTypes.string.isRequired,
       file: PropTypes.string.isRequired,
     }).isRequired,
     isListView: PropTypes.bool.isRequired,
-};
+  };
 
 export default Word;
