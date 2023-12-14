79c7102bc1446c0cbcd8dbee66b9a906ef86729f
Trev004 <71028998+Trev004@users.noreply.github.com>
2023-12-14 09:24:03 -0800

    Make it so clicking on span shows the title
    *Should* hopefully fix the issue that was happening if the title sometimes wasn't showing? I'm thinking maybe the laptop was thinking there was a click (which hides title tags) when there wasn't?

--- Wuikinuxv Learning App/src/assets/Components/Word.jsx
+++ Wuikinuxv Learning App/src/assets/Components/Word.jsx
@@ -1,10 +1,12 @@
 import { PropTypes } from "prop-types";
 import wordsJSON from '../words.json'
 import './Word.css'
+import { useEffect, useState } from "react";
 
 function Word ({ word, isListView }) {
     const { sounds, meaning, file } = word; 
     const baseSounds = wordsJSON.baseSounds;
+    const [activeSpan, setActiveSpan] = useState(null);
 
     const getSoundTip = (sound) => {
         const soundData = baseSounds.find(
@@ -13,10 +15,28 @@ function Word ({ word, isListView }) {
         );
         return soundData ? soundData.tip : '';
     };
+
+    const handleSpanClick = (index) => {
+      setActiveSpan(index === activeSpan ? null : index);
+    };
+
+    // GPT-Assisted, This checks if the user clicked the span, displaying the title if they did
+    // while removing it if they click anywhere else on the page
+    useEffect(() => {
+      const handleClickOutside = (event) => {
+        if (activeSpan !== null && !event.target.closest('.word-part-active')) {
+          setActiveSpan(null);
+        }
+      };
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+        document.removeEventListener('click', handleClickOutside);
+      };
+    }, [activeSpan]);
       
     return (
       <div className={isListView ? 'word-list' : 'word-card'}>
-        <h3>Word: {sounds.map((sound, index) => <span key={index} className="word-part" title={getSoundTip(sound)}>{sound}</span>)}</h3>
+        <h3>Word: {sounds.map((sound, index) => <span key={index} className={`word-part${index === activeSpan ? ' word-part-active' : ''}`} title={getSoundTip(sound)} onClick={() => handleSpanClick(index)}>{sound}</span>)}</h3>
         <p><i>Meaning:</i> {meaning}</p>
         <audio controls>
           <source src={file} type="audio/mpeg" />
